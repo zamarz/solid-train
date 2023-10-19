5763fcddb7d3243134496721c9c7dc2f73ae34e4
zamarz <zachmarzouk@gmail.com>
2023-10-19 09:28:40 +0100

    "Create basic design of calculator"

---
+++ src/components/NumbersGrid.tsx
@@ -0,0 +1,31 @@
+const NumbersGrid = () => {
+  return (
+    <div>
+      <h2>Hello World!</h2>
+
+      <div>
+        <button>0</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+      <div>
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+      </div>
+      <div>
+        <button>Enter</button>
+      </div>
+    </div>
+  );
+};
+
+export default NumbersGrid;
